880881af50eae2adeec214288339dd53f003428b
Michael Elfassy <mike.elfassy@gmail.com>
2022-11-13 20:41:58 -0500

    Updated overall style. Added non-functionial js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
+const input = document.getElementsByTagName('button')[0];
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
